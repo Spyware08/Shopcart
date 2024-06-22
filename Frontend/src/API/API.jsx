ce6ddddb608ed2a9da18eebe8430bd34dda84226
Spyware08 <0699gurpreet@gmail.com>
2024-06-22 10:39:49 +0530

    Updated the default login Login Credientials

--- Frontend/src/API/API.jsx
+++ Frontend/src/API/API.jsx
@@ -1,12 +1,13 @@
 import axios from "axios";
 const API = axios.create({
     baseURL: "https://shopcart-backend-pearl.vercel.app",
+    // baseURL: "http://192.168.224.94:8080",
 
 
     timeout: 10000, // 10 seconds in milliseconds
     headers: {
         "Content-Type": "application/json",
-        // You can add other headers as needed
+        
     },
     withCredentials: true
 });
--- Frontend/src/Component/ReuseComponent/Login.jsx
+++ Frontend/src/Component/ReuseComponent/Login.jsx
@@ -1,84 +1,105 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { FaShopify } from "react-icons/fa6";
-import { useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import API from '../../API/API';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 export default function LoginAccount() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  const log_useremail = useRef()
-  const log_password = useRef()
+  const [email, setEmail] = useState('demouser@gmail.com'); 
+  const [password, setPassword] = useState('12345678'); 
 
   async function handleSubmit(e) {
     e.preventDefault();
-    if (log_useremail.current && log_password.current) {
+    if (email && password) {
       let log_data = {
-        email: log_useremail.current.value,
-        password: log_password.current.value
+        email: email,
+        password: password
       };
       console.log("log data is", log_data);
 
       try {
         const res = await API.post("/login", log_data);
         sessionStorage.setItem("userData", JSON.stringify(res.data.userdata));
-          return navigate("/");
+        return navigate("/");
 
       } catch (e) {
         console.error("e", e.response.status);
         if (e.response.status === 401) {
-          // Handle wrong password scenario
           toast.info("Invalid Password");
         } else if (e.response.status === 404) {
           toast.info("User Not Found");
         } else {
-          // Handle other status codes if needed
           toast.error("Please Fill all Details");
         }
-
       }
     }
   }
 
   return (
     <div className='border-spacing-1 rounded-xl m-10 max-[450px]:h-screen max-[450px]:m-0'>
-
       <div className="flex flex-col justify-center py-10 px-12 border max-[450px]:px-5">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <h3 className='flex items-center text-green-800 text-2xl font-bold justify-center'><FaShopify />ShopCart</h3>
           <p className='text-center text-sm text-gray-800'>Buy Top Brands Cloths and Gadgets Here.</p>
           <h2 className="mt-5 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Login to your account</h2>
         </div>
-
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-6"  >
+          <form className="space-y-6">
             <div>
               <label className="block text-sm font-medium leading-6 text-gray-900">User email</label>
               <div className="mt-2">
-                <input id="email" name="email" type="email" ref={log_useremail} required className="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                <input 
+                  id="email" 
+                  name="email" 
+                  type="email" 
+                  value={email} 
+                  onChange={(e) => setEmail(e.target.value)} 
+                  required 
+                  className="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" 
+                />
               </div>
             </div>
-
             <div>
               <div className="flex items-center justify-between">
                 <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">Password</label>
-
               </div>
               <div className="mt-2">
-                <input id="password" name="password" type="password" ref={log_password} autoComplete="current-password" required className="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                <input 
+                  id="password" 
+                  name="password" 
+                  type="password" 
+                  value={password} 
+                  onChange={(e) => setPassword(e.target.value)} 
+                  autoComplete="current-password" 
+                  required 
+                  className="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" 
+                />
               </div>
             </div>
-
             <div>
-              <button type="submit" onClick={handleSubmit} className="flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button> <br />
-              <button  onClick={()=>{navigate("/")}} className="flex w-full justify-center rounded-md text-black bg-white-600 px-3 py-1.5 text-sm font-semibold leading-6shadow-sm hover:border hover:bg-blue-200">Cancel</button>
+              <button 
+                type="submit" 
+                onClick={handleSubmit} 
+                className="flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Sign in
+              </button> 
+              <br />
+              <button 
+                onClick={() => { navigate("/") }} 
+                className="flex w-full justify-center rounded-md text-black bg-white-600 px-3 py-1.5 text-sm font-semibold leading-6 shadow-sm hover:border hover:bg-blue-200"
+              >
+                Cancel
+              </button>
             </div>
           </form>
         </div>
       </div>
-      <ToastContainer className="max-[450px]:w-[15rem] max-[450px]:ml-[170px] max-[450px]:mt-2"
+      <ToastContainer 
+        className="max-[450px]:w-[15rem] max-[450px]:ml-[170px] max-[450px]:mt-2"
         position="top-right"
         autoClose={1500}
         hideProgressBar={false}
@@ -89,7 +110,7 @@ export default function LoginAccount() {
         draggable
         pauseOnHover
         theme="colored"
-        transition:Slide
+        transition="Slide"
       />
     </div>
   )
